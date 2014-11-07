8d5af686aeaa42300aaac1c9df3b186ae725ab93
helin24 <helin.shiah@gmail.com>
2014-11-07 11:10:34 -0600

    finished min cut algorithM

--- week-3/min_cut.rb
+++ week-3/min_cut.rb
@@ -1,12 +1,29 @@
 
 
-def min_cut(graph)
+def min_cut(graph_file, trials)
+	crossings = []
+	trials.times do |i|
+		puts "iteration #{i}"
+		graph = Graph.new(graph_file)
+		until graph.vertices.length == 2
+			edge_pair = graph.edges.sample
+			# puts "edge pair is #{edge_pair}"
+			graph.collapse(edge_pair)
+		end
+		crossings << graph.edges.length / 2
+	end	
+	puts crossings.inspect
+	crossings.min
 	# choose random edge
 	# for the two vertices
 	# collapse the two vertices together
+	# repeat until only two vertices remain
+	# then count how many edges between them
 end
 
 class Graph
+	attr_reader :vertices
+
 	def initialize(file)
 		@vertices = {}
 		f = File.open(file)
@@ -20,28 +37,24 @@ class Graph
 		edge_pairs = []
 		@vertices.each_pair do |v, edges|
 			edges.each do |e| 
-				edge_pairs << [v, e] if !edge_pairs.include?([e,v])
+				edge_pairs << [v, e]
 			end
 		end
-		edge_pairs
+		edge_pairs # will give repeat pairs, e.g. [1,4] and [4,1]
 	end
 	
 	def collapse(edge_pair)
-		# find the first one in vertices
-		# save its array of connections
-		# find second one in vertices
-		# save its array of connections
-		# make a new vertex that is [first vertex, second vertex]
-		# new vertex points to all of the connections from the two
 		merged_edges = @vertices[edge_pair[0]] + @vertices[edge_pair[1]]
-		@vertices.merge!(edge_pair => merged_edges)
-		edge_pair.each do |edge|
-			@vertices.delete(edge)
+		edge_pair.each do |vertex|
+			@vertices.delete(vertex)
 		end
-		@vertices		
-		# remove self join
-		# remove the old vertices
+		@vertices.merge!(edge_pair => merged_edges)
 
+		@vertices.each_pair do |vertex, edges|
+			edges.map!{|edge| edge == edge_pair[0] || edge == edge_pair[1] ? edge_pair : edge }
+		end
+
+		@vertices[edge_pair].delete(edge_pair)
 	end
 end
 
