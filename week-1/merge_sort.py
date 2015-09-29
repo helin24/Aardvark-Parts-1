2e7c4af24abd07fcd7bf9529f81c0be582514fdf
helin24 <helin.shiah@gmail.com>
2015-09-29 17:56:53 -0400

    merge sort

---
+++ week-1/merge_sort.py
@@ -0,0 +1,28 @@
+import math
+
+def merge_sort(array):
+    # if array length is greater than 2 then split into two halves and call merge_osrt on them
+    if (len(array) == 2):
+        return [min(array), max(array)]
+    elif (len(array) == 1):
+        return array
+    
+    split_index = int(math.ceil(len(array) / 2))
+    left = merge_sort(array[:split_index])
+    right = merge_sort(array[split_index:])
+
+    final = []
+
+    while (len(left) > 0 and len(right) > 0):
+        if (left[0] < right[0]):
+            final.append(left.pop(0))
+        else:
+            final.append(right.pop(0))
+
+    final.extend(left)
+    final.extend(right)
+
+    return final
+
+print merge_sort([3,5,76,3,2,3,4,5])
+print merge_sort([9,8,7,6,5,4,3,2,1])
