d8ab4c1dfceca49e630e669189d74af8d0c81d6a
helin24 <helin.shiah@gmail.com>
2015-09-29 21:30:47 -0400

    more correct inversion count

--- week-1/count_inversions.py
+++ week-1/count_inversions.py
@@ -1,31 +1,31 @@
 def count_inversions(alist):
-    # if size of list is 1, then return list with 0 inversions
-    # if there are two items in list and rigth goes before left then count as an inversion
-    # add inversions from components
-
-    if (len(alist) == 1):
-            return [alist, 0]
+    if len(alist) == 1:
+            return alist, 0
 
     split_index = len(alist) // 2
-    left = count_inversions(alist[:split_index])
-    right = count_inversions(alist[split_index:])
-    left_list = left[0]
-    right_list = right[0]
+    left_list, left_inversions = count_inversions(alist[:split_index])
+    right_list, right_inversions = count_inversions(alist[split_index:])
+    return merge_list(left_list, right_list, left_inversions + right_inversions)
+
+def merge_list(left_list, right_list, inversions):
+    left_index = 0
+    right_index = 0
     final = []
-    inversions = left[1] + right[1]
 
-    while (len(left_list) > 0 and len(right_list) > 0):
-        if (left_list[0] <= right_list[0]):
-            final.append(left_list.pop(0))
+    # was using pop but this is O(n) from having to move each element left by one space
+    while left_index < len(left_list) and right_index < len(right_list):
+        if left_list[left_index] <= right_list[right_index]:
+            final.append(left_list[left_index])
+            left_index += 1
         else:
-            final.append(right_list.pop(0))
-            inversions += len(left_list)
+            final.append(right_list[right_index])
+            right_index += 1
+            inversions += len(left_list) - left_index
 
-    final.extend(left_list)
-    final.extend(right_list)
+    final.extend(left_list[left_index:])
+    final.extend(right_list[right_index:])
 
-    print [final, inversions]
     return [final, inversions]
 
-count_inversions([12, 11])
-count_inversions([9,8,7,6,5,4,3,2,1])
+print count_inversions([12, 11])
+print count_inversions([9,8,7,6,5,4,3,2,1])
