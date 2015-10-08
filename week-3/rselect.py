78a2ed569c912077ea9e8bda76a052be36b9501e
helin24 <helin.shiah@gmail.com>
2015-10-07 20:13:25 -0400

    rselect removing print messages

--- week-3/rselect.py
+++ week-3/rselect.py
@@ -1,10 +1,6 @@
 import random
 
 def rselect(alist, index):
-    # pick random pivot
-    # move pivot to beginning
-    # for all indexes in array move to last lower if lower than pivot
-    # if pivot is equal to 
     end = len(alist) - 1
 
     if index > end:
@@ -14,7 +10,6 @@ def rselect(alist, index):
         return alist[0]
 
     pivot_index = random.randint(0, end)
-    print "pivot index is %s" % (pivot_index)
 
     alist[0], alist[pivot_index] = alist[pivot_index], alist[0]
 
@@ -26,9 +21,6 @@ def rselect(alist, index):
             alist[last_lower], alist[i] = alist[i], alist[last_lower]
             last_lower += 1
 
-    print "last_lower is %s" % (last_lower)
-    print alist
-
     if last_lower - 1 == index:
         return pivot
     elif last_lower - 1 > index:
@@ -36,7 +28,7 @@ def rselect(alist, index):
     elif last_lower - 1 < index:
         return rselect(alist[last_lower : end + 1], index - last_lower)
 
-# print rselect([3,8,2,5,1,4,7,6], 3)
+print rselect([3,8,2,5,1,4,7,6], 3)
 print rselect([3,8,2,5,1,4,7,6], 5)
  
 print rselect([3,8,2,5,1,4,7,6], 9)
