21960b5e1a57c02e08a5fa8d10652e6519c044a7
helin24 <helin.shiah@gmail.com>
2015-10-13 16:13:13 -0400

    min cut works but messy

--- week-3/min_cut.py
+++ week-3/min_cut.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 def min_cut(graph):
     return
@@ -15,25 +16,28 @@ class Graph:
         min_so_far = self.num_edges()
         for i in range(repetitions):
             single_min = self.single_min_cut()
+            print "single min is %s" % (single_min)
             min_so_far = min(min_so_far, single_min)
         return min_so_far
 
     def single_min_cut(self):
-        assoc_arr = self.assoc_arr
+        assoc_arr = copy.deepcopy(self.assoc_arr)
 
         while len(assoc_arr) > 2:
             current_pair = random.choice(self.all_edges(assoc_arr))
-            print current_pair
+#            print current_pair
             merged_edges = assoc_arr[current_pair[0]]
 
             merged_edges.extend(assoc_arr[current_pair[1]])
-            print merged_edges
             external_edges = []
             for edge in merged_edges:
                 if edge != current_pair[0] and edge != current_pair[1]:
                     external_edges.append(edge)
             
-            new_node = (current_pair[0],) + (current_pair[1],)
+            combined_nodes = []
+            combined_nodes.extend(list(current_pair[0]))
+            combined_nodes.extend(list(current_pair[1]))
+            new_node = tuple(combined_nodes)
             assoc_arr[new_node] = external_edges
    
             for edge in set(external_edges):
@@ -44,13 +48,10 @@ class Graph:
             assoc_arr.pop(current_pair[0], None)
             assoc_arr.pop(current_pair[1], None)
 
-            print assoc_arr
-           
+#            print assoc_arr
 
-        # pick random edge to remove and bring other node into one node
-        # merge nodes by making a set of nodes with the combined edges of both nodes, delete the individual nodes, then go through all edges and change in other nodes to reference set instead of individual nodes
-        # when only two nodes then count number of edges in one side
-    
+        return len(assoc_arr[new_node])
+           
     def num_edges(self):
         total = 0
         for node in self.assoc_arr:
@@ -62,13 +63,12 @@ class Graph:
         for value in assoc_arr:
             for partner in assoc_arr[value]:
                 edges.append([value, partner])
-        print "there are currently %s edges" % (len(edges))
+#        print "there are currently %s edges" % (len(edges))
         return list(edges)
 
 
 # answer to problem given in kargerMinCut.txt is 17
 
 graph = Graph('kargerMinCut.txt')
-graph.single_min_cut()
-exit()
-graph.min_cut(10)
+print graph.min_cut(20)
+
